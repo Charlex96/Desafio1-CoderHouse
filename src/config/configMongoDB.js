7014d9a81f76c2ed7b8301430f251617fffc5483
Charlex96 <fimacharles@hotmail.com>
2024-03-12 19:58:39 -0500

    8va pre

--- src/config/configMongoDB.js
+++ src/config/configMongoDB.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 const config = {
     mongoDB: {
         // URL: `mongodb+srv://fimacharles:lE8oqWVO44hcDsJu@dbcoder.bpexbss.mongodb.net/ecommerce`, // Use environment variables for sensitive data
+        
         URL: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@dbcoder.bpexbss.mongodb.net/${process.env.DB_NAME}`, // Use environment variables for sensitive data
     },
 };
