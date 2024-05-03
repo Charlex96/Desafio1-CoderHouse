9739f45276924f62ed8363abe816241fe2cc94c8
Charlex96 <fimacharles@hotmail.com>
2024-05-02 22:30:16 -0500

    Mocking y manejo de errores

---
+++ src/tests/loginTests.js
@@ -0,0 +1,44 @@
+// Importamos la función a testear
+
+const login = require("../login")
+
+console.log(`Test 1: Si se pasa un password vacío, la función debe consologuear (“No se ha proporcionado un password”)`)
+const resultTest1 = login('fimacharles@gmail.com')
+if(resultTest1 === 'No se ha proporcionado un password'){
+    console.log(`   👌 SUCCESS Test 1`)
+}else{
+    console.log(`   ❌ FAIL Test 1`)
+}
+
+console.log(`Test 2: Si se pasa un usuario vacío, la función debe consologuear (“No se ha proporcionado un usuario”)`)
+const resultTest2 = login()
+if(resultTest2 === 'No se ha proporcionado un usuario'){
+    console.log(`   👌 SUCCESS Test 2`)
+}else{
+    console.log(`   ❌ FAIL Test 2`)
+}
+
+
+console.log(`Test 3: Si se pasa un password incorrecto, consologuear (“Contraseña incorrecta”)`)
+const resultTest3 = login('fimacharles@gmail.com', 123)
+if(resultTest3 === 'Contraseña incorrecta'){
+    console.log(`   👌 SUCCESS Test 3`)
+}else{
+    console.log(`   ❌ FAIL Test 3`)
+}
+
+console.log(`Test 4: Si se pasa un usuario incorrecto, consologuear (“Credenciales incorrectas”)`)
+const resultTest4 = login('fimacharles@gmail.com', 123)
+if(resultTest4 === 'Credenciales incorrectas'){
+    console.log(`   👌 SUCCESS Test 4`)
+}else{
+    console.log(`   ❌ FAIL Test 4`)
+}
+
+console.log(`Test 5: Si  el usuario y contraseña coinciden, consologuear (“logueado”)`)
+const resultTest5 = login('fimacharles@gmail.com', 1234)
+if(resultTest5 === 'logueado'){
+    console.log(`   👌 SUCCESS Test 5`)
+}else{
+    console.log(`   ❌ FAIL Test 5`)
+}
--- src/utils/fakerData.js
+++ src/utils/fakerData.js
@@ -1,5 +1,13 @@
 import { faker } from "@faker-js/faker";
 
 export const getRandomCode = () => {
-  return faker.string.alphanumeric(5);
-};
+  return {
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        stock: faker.number.int({ min: 10, max: 100 }),
+        image: faker.image.url(),
+        id: faker.database.mongodbObjectId(),
+        code: faker.string.alphanumeric(100),
+        description: faker.lorem.paragraph()
+    }
+  };
